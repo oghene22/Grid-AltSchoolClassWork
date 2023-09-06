06d682c3c088ec0a0e345e12ceea6c5d62c4a4b5
OgheneO'Tega Abraham <ogheneotegaabraham@gmail.com>
2023-09-06 16:27:37 +0000

    Assignment

--- src/utils.js
+++ src/utils.js
@@ -4,10 +4,20 @@ export const range = (start, end, step = 1) => {
 
   // handle scenario where end is not specified range(5) - [0, 1, 2, 3, 4]
   // a conditional check for end
-
-  for (let i = start; i < end; i += step) {
-    // loop code
+  if(end !== undefined){
+    for(let i = 0; i < start; i += step){
+      let row = []
+      for (let j = 0; j < end; j += step) {
+        row.push(null)
+      }
+      output.push(row)
+    }
+  } else {
+    for(let i = 0; i < start; i += step) {
+      output.push(null)
+    }
   }
+
 
   return output;
 };
